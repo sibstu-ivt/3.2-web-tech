2304129a62706d259973c05fdba12ea622130903
NumminorihSF <numminorihsf@gmail.com>
2015-12-01 07:25:35 +0700

    lab6 works

--- lab6.js
+++ lab6.js
@@ -21,43 +21,21 @@
       '</div>';
     document.body.innerHTML = innerHtml;
 
+    function onHover (pic){
+        document.getElementById('img').src=pic;
+    }
+    function onLeave (){
+        document.getElementById('img').src='empty-1.jpg';
+    }
 
-    document.getElementById("hip").addEventListener('mouseEnter', function(){
+    function addListeners (id, pic){
+      document.getElementById(id).addEventListener('mouseenter', onHover.bind(null, pic));
+      document.getElementById(id).addEventListener('mouseleave', onLeave);
+    }
 
-    });
-
-    document.getElementById("show").addEventListener('mouseEnter', function(){
-      var name = document.getElementById('select').value;
-      var portrait = name + '.jpg';
-      var backgrounds = document.getElementsByClassName('back-radio');
-      var backgroundChecked = null;
-      for(var i=0; i < backgrounds.length; i++){
-        if (backgrounds[i].checked){
-          backgroundChecked = backgrounds[i].id;
-        }
-      }
-      if (backgroundChecked === null){
-        throw new Error('Need check background!');
-      }
-      var background = backgroundChecked + '.jpg';
-
-      var style = 'font-style:' +
-        (document.getElementById('text-cursive').checked ?'italic':'normal') + ';' +
-        'font-weight:' +
-        (document.getElementById('text-bold').checked ?'bold':'normal') + ';' +
-        'text-decoration:' +
-        (document.getElementById('text-underline').checked ?'underline':'none') + ';';
-
-      var text = '<!DOCTYPE html>' +
-        '<html><head></head><body style="background:' + background + '">' +
-        '<h1 style="'+ style +'">'+name+'</h1>' +
-        '<img src="./'+portrait+'"/>' +
-        '<p style="'+ style + '">'+ document.getElementById('sign').value + '</p>' +
-        '<button onclick="window.close()">Закрыть</button></body></html>';
-
-      var newW = window.open();
-      newW.document.write(text);
-    });
+    addListeners('hip', 'gippopotam-1.jpg');
+    addListeners('ack', 'akula-1.jpg');
+    addListeners('mam', 'hobot-1.jpg');
   });
 
 })();
